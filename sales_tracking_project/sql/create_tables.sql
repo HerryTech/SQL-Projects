1b0d776c7498a18fc1fb7e3a31b617f68f80d053
Blessing <blessingilesanmi21@gmail.com>
2025-05-08 15:19:01 +0100

    Create create_table.sql

---
+++ sales_tracking_project/sql/create_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE customers(
+customer_id INT PRIMARY KEY,
+customer_name VARCHAR(100) NOT NULL,
+email VARCHAR(100) UNIQUE,
+city VARCHAR(50),
+country VARCHAR(50)
+);
+
+CREATE TABLE products(
+product_id INT PRIMARY KEY,
+product_name VARCHAR(100) NOT NULL,
+category VARCHAR(50),
+price DECIMAL(10, 2)
+);
+
+CREATE TABLE sales(
+sale_id INT PRIMARY KEY,
+customer_id INT,
+product_id INT,
+sale_date DATE,
+quantity INT,
+total_amount DECIMAL(10,2),
+FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+FOREIGN KEY (product_id) REFERNCES products(product_id)
+);
