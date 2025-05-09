eb40639862de4b328d1ee9d2937d72b24a4686a6
Blessing <blessingilesanmi21@gmail.com>
2025-05-09 10:49:59 +0100

    update create_tables script

--- sales_tracking_project/sql/create_tables.sql
+++ sales_tracking_project/sql/create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE customers(
-customer_id INT PRIMARY KEY,
+customer_id VARCHAR PRIMARY KEY,
 customer_name VARCHAR(100) NOT NULL,
 email VARCHAR(100) UNIQUE,
 city VARCHAR(50),
@@ -7,14 +7,14 @@ country VARCHAR(50)
 );
 
 CREATE TABLE products(
-product_id INT PRIMARY KEY,
+product_id VARCHAR PRIMARY KEY,
 product_name VARCHAR(100) NOT NULL,
 category VARCHAR(50),
 price DECIMAL(10, 2)
 );
 
 CREATE TABLE sales(
-sale_id INT PRIMARY KEY,
+sale_id VARCHAR PRIMARY KEY,
 customer_id INT,
 product_id INT,
 sale_date DATE,
