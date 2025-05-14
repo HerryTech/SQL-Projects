4768065c102672170741ea8a8d04f3cfd295dd59
Blessing <blessingilesanmi21@gmail.com>
2025-05-14 11:34:54 +0100

    update queries script

--- sales_tracking_project/sql/queries.sql
+++ sales_tracking_project/sql/queries.sql
@@ -8,4 +8,13 @@ JOIN customers c ON c.customer_id = s.customer_id
 GROUP BY c.customer_name;
 
 -- This query calculates the total sales for each product
-SELECT p.product
+SELECT p.product_name, SUM (s.total_amount) AS product_sales
+FROM sales s
+JOIN products p ON p.product_id = s.product_id
+GROUP BY p.product_name;
+
+-- This query calculates quantity sold per product
+SELECT p.product_name, SUM(s.quantity) AS total_quantity_sold
+FROM sales s
+JOIN products p ON p.product_id = s.product_id
+GROUP BY p.product_name;
