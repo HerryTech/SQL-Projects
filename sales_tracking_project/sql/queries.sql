f1baf94fc76c9c75940a25e0b25d311dea8dc364
Blessing <blessingilesanmi21@gmail.com>
2025-05-14 11:03:03 +0100

    update queries script

--- sales_tracking_project/sql/queries.sql
+++ sales_tracking_project/sql/queries.sql
@@ -1,3 +1,8 @@
 -- This query calculates the total sales amount from the sales table
 SELECT SUM(total_amount) AS total_sales FROM sales;
 
+-- This query gets total amount spent by each customer
+SELECT c.customer_name, SUM (s.total_amount) AS total_spent
+FROM sales s
+JOIN customers c ON c.customer_id = s.customer_id
+
