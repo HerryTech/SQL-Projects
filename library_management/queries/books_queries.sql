364930f7c3d1abfdd544201456cbd079feb6f078
Blessing <blessingilesanmi21@gmail.com>
2025-05-22 11:50:12 +0100

    Update queries

--- library_management/queries/books_queries.sql
+++ library_management/queries/books_queries.sql
@@ -12,3 +12,9 @@ FROM books
 WHERE publish_year < 2000;
 
 --Top 5 most issued books
+SELECT b.title, COUNT(ir.issue_id) AS issue_count
+FROM issue_records ir
+JOIN books b ON b.book_id = ir.book_id
+GROUP BY b.title
+ORDER BY issue_count DESC
+LIMIT 5;
