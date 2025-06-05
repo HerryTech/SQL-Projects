7912ad95c621a56d825f1cf304e0a4381cd56635
Blessing <blessingilesanmi21@gmail.com>
2025-06-05 15:47:00 +0100

    Update create_tables script

--- Ben_pizza/schema/create_tables.sql
+++ Ben_pizza/schema/create_tables.sql
@@ -1,10 +1,10 @@
-﻿CREATE TABLE orders(
-row_id INT PRIMARY KEY,
-order_id INT,
+﻿Create table orders(
+row_id int primary key,
+order_id int,
 created_at timestamp,
 item_id varchar(10),
-cust_id INT,
-add_id int
+cust_id int,
+add_id int,
 FOREIGN KEY(cust_id) REFERENCES customers(cust_id),
 FOREIGN KEY(add_id) REFERENCES address(item_id)
 );
