a98ea73b7ea36cfbedf609edad5d16a77c72cb4c
Blessing <blessingilesanmi21@gmail.com>
2025-06-05 14:51:30 +0100

    Create create_tables script

--- Ben_pizza/schema/create_tables.sql
+++ Ben_pizza/schema/create_tables.sql
@@ -1,142 +1,83 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/S0Ob58
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+﻿Create table():
+row_id int PK
+order_id int
+created_at timestamp
+item_id varchar(10) FK >- item.item_id
+cust_id int FK >- Customer.cust_id
+add_id int FK >- address.add_id
+
+Create table():Customer
+-
+cust_id int pk
+cust_firstname varchar(50)
+cust_secondname varchar(50)
+
+Create table():address
+-
+add_id int pk
+delivery boolean
+delivery_address1 varchar(200)
+delivery_address2 varchar(200) NULL
+delivery_city varchar(20)
+delivery_zipcode int
+
+Create table():item
+-
+item_id varchar(10) pk
+sku varchar(20) FK >- recipe.recipe_id
+item_name varchar(100)
+item_cat varchar(100)
+item_size varchar(10)
+item_price decimal(5,2)
+
+Create table():ingredient
+-
+ing_id varchar(10) pk
+ing_name varchar(100)
+ing_weight int
+ing_meas varchar(20)
+ing_price decimal(5,2)
+
+Create table():recipe
+-
+row_id int pk
+recipe_id varchar(20) UNIQUE
+ing_id varchar(20) FK >- ingredient.ing_id
+quantity int
+
+Create table():inventory
+-
+inv_id int pk
+item_id varchar(10) FK >- recipe.ing_id
+quantity int
+
+Create table():staff
+-
+staff_id varchar(10) pk
+first_name varchar(100)
+last_name varchar(100)
+position varchar(100)
+hourly_rate decimal(5,2)
+
+Create table():shift
+-
+shift_id varchar(10) pk
+day_of_week varchar(20)
+start_time timestamp
+end_time timestamp
+
+Create table():roster
+-
+row_id int pk
+rota_id varchar(10)
+date date FK >- Order.created_at
+shift_id varchar(10) FK >- shift.shift_id
+staff_id varchar(10) FK >- staff.staff_id
 
 
-CREATE TABLE "Order" (
-    "row_id" int   NOT NULL,
-    "order_id" int   NOT NULL,
-    "created_at" timestamp   NOT NULL,
-    "item_id" varchar(10)   NOT NULL,
-    "cust_id" int   NOT NULL,
-    "add_id" int   NOT NULL,
-    CONSTRAINT "pk_Order" PRIMARY KEY (
-        "row_id"
-     )
-);
 
-CREATE TABLE "Customer" (
-    "cust_id" int   NOT NULL,
-    "cust_firstname" varchar(50)   NOT NULL,
-    "cust_secondname" varchar(50)   NOT NULL,
-    CONSTRAINT "pk_Customer" PRIMARY KEY (
-        "cust_id"
-     )
-);
 
-CREATE TABLE "address" (
-    "add_id" int   NOT NULL,
-    "delivery" boolean   NOT NULL,
-    "delivery_address1" varchar(200)   NOT NULL,
-    "delivery_address2" varchar(200)   NULL,
-    "delivery_city" varchar(20)   NOT NULL,
-    "delivery_zipcode" int   NOT NULL,
-    CONSTRAINT "pk_address" PRIMARY KEY (
-        "add_id"
-     )
-);
 
-CREATE TABLE "item" (
-    "item_id" varchar(10)   NOT NULL,
-    "sku" varchar(20)   NOT NULL,
-    "item_name" varchar(100)   NOT NULL,
-    "item_cat" varchar(100)   NOT NULL,
-    "item_size" varchar(10)   NOT NULL,
-    "item_price" decimal(5,2)   NOT NULL,
-    CONSTRAINT "pk_item" PRIMARY KEY (
-        "item_id"
-     )
-);
 
-CREATE TABLE "ingredient" (
-    "ing_id" varchar(10)   NOT NULL,
-    "ing_name" varchar(100)   NOT NULL,
-    "ing_weight" int   NOT NULL,
-    "ing_meas" varchar(20)   NOT NULL,
-    "ing_price" decimal(5,2)   NOT NULL,
-    CONSTRAINT "pk_ingredient" PRIMARY KEY (
-        "ing_id"
-     )
-);
 
-CREATE TABLE "recipe" (
-    "row_id" int   NOT NULL,
-    "recipe_id" varchar(20)   NOT NULL,
-    "ing_id" varchar(20)   NOT NULL,
-    "quantity" int   NOT NULL,
-    CONSTRAINT "pk_recipe" PRIMARY KEY (
-        "row_id"
-     ),
-    CONSTRAINT "uc_recipe_recipe_id" UNIQUE (
-        "recipe_id"
-    )
-);
-
-CREATE TABLE "inventory" (
-    "inv_id" int   NOT NULL,
-    "item_id" varchar(10)   NOT NULL,
-    "quantity" int   NOT NULL,
-    CONSTRAINT "pk_inventory" PRIMARY KEY (
-        "inv_id"
-     )
-);
-
-CREATE TABLE "staff" (
-    "staff_id" varchar(10)   NOT NULL,
-    "first_name" varchar(100)   NOT NULL,
-    "last_name" varchar(100)   NOT NULL,
-    "position" varchar(100)   NOT NULL,
-    "hourly_rate" decimal(5,2)   NOT NULL,
-    CONSTRAINT "pk_staff" PRIMARY KEY (
-        "staff_id"
-     )
-);
-
-CREATE TABLE "shift" (
-    "shift_id" varchar(10)   NOT NULL,
-    "day_of_week" varchar(20)   NOT NULL,
-    "start_time" timestamp   NOT NULL,
-    "end_time" timestamp   NOT NULL,
-    CONSTRAINT "pk_shift" PRIMARY KEY (
-        "shift_id"
-     )
-);
-
-CREATE TABLE "roster" (
-    "row_id" int   NOT NULL,
-    "rota_id" varchar(10)   NOT NULL,
-    "date" date   NOT NULL,
-    "shift_id" varchar(10)   NOT NULL,
-    "staff_id" varchar(10)   NOT NULL,
-    CONSTRAINT "pk_roster" PRIMARY KEY (
-        "row_id"
-     )
-);
-
-ALTER TABLE "Order" ADD CONSTRAINT "fk_Order_item_id" FOREIGN KEY("item_id")
-REFERENCES "item" ("item_id");
-
-ALTER TABLE "Order" ADD CONSTRAINT "fk_Order_cust_id" FOREIGN KEY("cust_id")
-REFERENCES "Customer" ("cust_id");
-
-ALTER TABLE "Order" ADD CONSTRAINT "fk_Order_add_id" FOREIGN KEY("add_id")
-REFERENCES "address" ("add_id");
-
-ALTER TABLE "item" ADD CONSTRAINT "fk_item_sku" FOREIGN KEY("sku")
-REFERENCES "recipe" ("recipe_id");
-
-ALTER TABLE "recipe" ADD CONSTRAINT "fk_recipe_ing_id" FOREIGN KEY("ing_id")
-REFERENCES "ingredient" ("ing_id");
-
-ALTER TABLE "inventory" ADD CONSTRAINT "fk_inventory_item_id" FOREIGN KEY("item_id")
-REFERENCES "recipe" ("ing_id");
-
-ALTER TABLE "roster" ADD CONSTRAINT "fk_roster_date" FOREIGN KEY("date")
-REFERENCES "Order" ("created_at");
-
-ALTER TABLE "roster" ADD CONSTRAINT "fk_roster_shift_id" FOREIGN KEY("shift_id")
-REFERENCES "shift" ("shift_id");
-
-ALTER TABLE "roster" ADD CONSTRAINT "fk_roster_staff_id" FOREIGN KEY("staff_id")
-REFERENCES "staff" ("staff_id");
 
