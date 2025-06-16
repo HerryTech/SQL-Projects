7c4abe989557f9bdb974497c51d482894b8f3c07
Blessing <blessingilesanmi21@gmail.com>
2025-06-16 11:27:57 +0100

    Update queries

--- Ben_pizza/SQL/queries.sql
+++ Ben_pizza/SQL/queries.sql
@@ -1,9 +1,14 @@
--- Customer Insights
+-- CUSTOMER INSIGHTS
 -- 1. List of customers
 Select * from customers;
 
 --2. Customers who have placed order
 select Distinct c.* 
 from customers c
-JOIN orders o on o.cust_id = c.cust_id;
+join orders o on o.cust_id = c.cust_id;
 
+--3. Total number of orders per customer
+select c.cust_id, count(o.order_id) as num_orders
+from customers c
+join orders o on o.cust_id = c.cust_id
+group by c.cust_id; 
