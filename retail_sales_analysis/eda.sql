d3d8cd625ec06c2ff20d70ec70ff885aae449245
Blessing <blessingilesanmi21@gmail.com>
2025-06-25 14:51:03 +0100

    Update EDA script

--- retail_sales_analysis/eda.sql
+++ retail_sales_analysis/eda.sql
@@ -2,10 +2,10 @@
 SELECT * FROM retail_sales
 LIMIT 10; 
 
--- check the total number of data
+-- Check the total number of data
 SELECT COUNT(*) FROM retail_sales;
 
---check for null values in all columns
+--Check for null values in all columns
 SELECT * FROM retail_sales
 WHERE transactions_id IS NULL
 OR sale_date IS NULL
@@ -18,4 +18,15 @@ OR price_per_unit IS NULL
 OR cogs IS NULL
 OR total_sale IS NULL;
 
-
+-- Delete rows with null
+DELETE FROM retail_sales
+WHERE transactions_id IS NULL
+OR sale_date IS NULL
+OR sale_time IS NULL
+OR customer_id IS NULL
+OR gender IS NULL
+OR category IS NULL
+OR quantiy IS NULL
+OR price_per_unit IS NULL
+OR cogs IS NULL
+OR total_sale IS NULL;
