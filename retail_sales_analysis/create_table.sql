634b5eb199abe435c4fca9127b4ad71300e8f5e0
Blessing <blessingilesanmi21@gmail.com>
2025-08-05 13:25:21 +0100

    Update Script

--- retail_sales_analysis/create_table.sql
+++ retail_sales_analysis/create_table.sql
@@ -1,13 +1,14 @@
 -- Create Table
 CREATE TABLE retail_sales(
-transactions_id INT PRIMARY KEY,
-sale_date DATE,
-sale_time TIME,
-customer_id	INT,
-gender	VARCHAR(15),
-age	INT,
-category VARCHAR(15),
-quantiy INT,
-price_per_unit FLOAT,
-cogs FLOAT,
-total_sale FLOAT);
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,
+    sale_time TIME,
+    customer_id	INT,
+    gender	VARCHAR(15),
+    age	INT,
+    category VARCHAR(15),
+    quantiy INT,
+    price_per_unit FLOAT,
+    cogs FLOAT,
+    total_sale FLOAT
+);
