4260bcd7964156b42c31be5c864e8a60eeb99ec6
Blessing <blessingilesanmi21@gmail.com>
2025-08-07 14:41:16 +0100

    Update Script

--- retail_sales_analysis/eda.sql
+++ retail_sales_analysis/eda.sql
@@ -53,10 +53,10 @@ SELECT DISTINCT category AS category FROM retail_sales;
 SELECT * FROM retail_sales
 WHERE sale_date = '2022-11-05';
 
--- 2. Retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
+-- 2. Retrieve all transactions where the category is 'Clothing' and the quantity sold is more than or equal to 4 in the month of Nov-2022
 SELECT * FROM retail_sales
 WHERE category = 'Clothing'
-AND quantity > 4
+AND quantity >= 4
 AND TO_CHAR(sale_date, 'YYYY-MM') = '2022-11';
 
 -- 3. Calculate the total order and total sales for each category.
