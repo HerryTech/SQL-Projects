174034dda274e6e524de51207feca85de5f235eb
Blessing <blessingilesanmi21@gmail.com>
2025-08-07 14:31:11 +0100

    Update Script

--- retail_sales_analysis/eda.sql
+++ retail_sales_analysis/eda.sql
@@ -62,4 +62,4 @@ AND TO_CHAR(sale_date, 'YYYY-MM') = '2022-11';
 -- 3. Calculate the total sales for each category.
 SELECT category, SUM(total_sale) AS total_sales 
 FROM retail_sales
-GROUP BY category;
+GROUP BY 1;
