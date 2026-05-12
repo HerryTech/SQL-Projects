552729a59701dea68734793b20473c93106295a6
Blessing <blessingilesanmi21@gmail.com>
2026-05-12 11:22:52 +0100

    Update scripts

--- Retail_SQL_Project/Sql_Scripts/create_tables.sql
+++ Retail_SQL_Project/Sql_Scripts/create_tables.sql
@@ -2,3 +2,8 @@ CREATE TABLE retail.categories(
 	category_id INT PRIMARY KEY,
 	category_name VARCHAR(20)
 );
+
+CREATE TABLE retail.suppliers (
+	supplier_id INT PRIMARY KEY,
+	country VARCHAR(20)
+);
