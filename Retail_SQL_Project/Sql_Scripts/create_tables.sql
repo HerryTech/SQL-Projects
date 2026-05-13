aeece490732351e4bc0dd689b17a5c921a0aa02c
Blessing <blessingilesanmi21@gmail.com>
2026-05-13 10:08:22 +0100

    Update

--- Retail_SQL_Project/Sql_Scripts/create_tables.sql
+++ Retail_SQL_Project/Sql_Scripts/create_tables.sql
@@ -23,3 +23,19 @@ CREATE TABLE retail.customers(
 	city VARCHAR(20),
 	signup_date DATE
 )
+
+CREATE TABLE retail.products(
+	product_id INT PRIMARY KEY,
+	category_id	INT,
+	supplier_id INT,
+	price NUMERIC(10,2),
+
+	CONSTRAINT fk_category
+		FOREIGN KEY (category_id)
+		REFERENCES retail.categories(category_id),
+
+	CONSTRAINT fk_supplier
+		FOREIGN KEY(supplier_id)
+		REFERENCES retail.suppliers(supplier_id)
+)
+
