b40a7c59927237a294e6f5fb7d6b84a795dcd1b0
Blessing <blessingilesanmi21@gmail.com>
2026-05-14 12:48:45 +0100

    Update Script

--- Retail_SQL_Project/Sql_Scripts/analysis.sql
+++ Retail_SQL_Project/Sql_Scripts/analysis.sql
@@ -7,6 +7,6 @@ SELECT
 SELECT c.category_name, (oi.qty * oi.price) AS total_sales
 FROM retail.order_items oi
 JOIN retail.products p
-	ON 	oi_product_id = p.product_id
-JOIN retail.category c
-	ON p.category_id = c.category_id
+	ON 	oi.product_id = p.product_id
+JOIN retail.categories c
+	ON p.category_id = c.category_id;
