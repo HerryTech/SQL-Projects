e3e2e70e9a948ff24e61f8efe18365252e886f62
Blessing <blessingilesanmi21@gmail.com>
2026-05-16 11:20:53 +0100

    Update Script

---
+++ Retail_SQL_Project/Sql_Scripts/analysis.sql
@@ -0,0 +1,14 @@
+-- Total Revenue
+SELECT 
+	SUM(qty * price) AS total_revenue
+	FROM retail.order_items;
+
+-- Sales by category
+SELECT c.category_name, SUM(oi.qty * oi.price) AS total_sales
+FROM retail.order_items oi
+JOIN retail.products p
+	ON 	oi.product_id = p.product_id
+JOIN retail.categories c
+	ON p.category_id = c.category_id
+GROUP BY c.category_name
+ORDER BY total_sales DESC;
