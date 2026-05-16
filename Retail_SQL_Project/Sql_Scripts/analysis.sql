cdd6034445fd7bbd429a4a03fe5042358a416ca7
Blessing <blessingilesanmi21@gmail.com>
2026-05-16 11:34:14 +0100

    Update Script

--- Retail_SQL_Project/Sql_Scripts/analysis.sql
+++ Retail_SQL_Project/Sql_Scripts/analysis.sql
@@ -14,7 +14,7 @@ GROUP BY c.category_name
 ORDER BY total_sales DESC;
 
 -- Revenue by Store
-SELECT 'store' AS table_name, SUM(oi.qty * oi.price) AS total_revenue)
+SELECT 'store' AS table_name, SUM(oi.qty * oi.price) AS total_revenue
 FROM retail.order_items oi
 JOIN retail.orders o
 	ON oi.order_id = o.order_id
