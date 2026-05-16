425db9f70a0ee5b6765f1b0f9db8f7dee9ec017d
Blessing <blessingilesanmi21@gmail.com>
2026-05-16 12:38:52 +0100

    Update Script

--- Retail_SQL_Project/Sql_Scripts/customer_analysis.sql
+++ Retail_SQL_Project/Sql_Scripts/customer_analysis.sql
@@ -1,2 +1,11 @@
 -- Total Customers
-SELECT COUNT(DISTINCT customer_id) AS total_customers FROM retail.customers;
+SELECT 
+	COUNT(DISTINCT customer_id) AS total_customers 
+FROM retail.customers;
+
+-- Customers by City
+SELECT
+	city, COUNT(DISTINCT customer_id) AS total_customers
+FROM retail.customers
+GROUP BY city
+ORDER BY total_customers DESC;
