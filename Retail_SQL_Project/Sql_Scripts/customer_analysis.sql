5247435c43c8c963d203987bacc44b4b26f0fc02
Blessing <blessingilesanmi21@gmail.com>
2026-05-16 12:47:28 +0100

    Update Script

--- Retail_SQL_Project/Sql_Scripts/customer_analysis.sql
+++ Retail_SQL_Project/Sql_Scripts/customer_analysis.sql
@@ -5,7 +5,19 @@ FROM retail.customers;
 
 -- Customers by City
 SELECT
-	city, COUNT(DISTINCT customer_id) AS total_customers
+	city, COUNT(customer_id) AS total_customers
 FROM retail.customers
 GROUP BY city
 ORDER BY total_customers DESC;
+
+-- Top 10 Customers by Revenue
+SELECT 
+	c.customer_id, SUM(oi.qty * oi.price) AS total_revenue
+FROM retail.order_items oi
+JOIN retail.orders o
+	ON oi.order_id = o.order_id
+JOIN retail.customers c
+	ON o.customer_id = c.customer_id
+GROUP BY c.customer_id
+ORDER BY total_revenue
+LIMIT 10;
