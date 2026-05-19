7d4f01714e47af341d3aaad45be6e49affb2eaf0
Blessing <blessingilesanmi21@gmail.com>
2026-05-19 12:26:23 +0100

    Update Script

--- Retail_SQL_Project/Sql_Scripts/returns_risk_analysis.sql
+++ Retail_SQL_Project/Sql_Scripts/returns_risk_analysis.sql
@@ -16,4 +16,17 @@ FROM retail.order_items oi
 LEFT JOIN retail.returns r
 	ON r.order_item_id = oi.order_item_id;
 
+-- Returns by Category
+SELECT
+    c.category_name,
+    COUNT(r.return_id) AS total_returns
+FROM retail.returns r
+JOIN retail.order_items oi
+    ON r.order_item_id = oi.order_item_id
+JOIN retail.products p
+    ON oi.product_id = p.product_id
+JOIN retail.categories c
+    ON p.category_id = c.category_id
+GROUP BY c.category_name
+ORDER BY total_returns DESC;
 
