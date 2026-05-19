9723c432c5fa93dbc64083be6af8707a5a3a4617
Blessing <blessingilesanmi21@gmail.com>
2026-05-19 14:09:48 +0100

    Update Script

--- Retail_SQL_Project/Sql_Scripts/time_series_analysis.sql
+++ Retail_SQL_Project/Sql_Scripts/time_series_analysis.sql
@@ -8,3 +8,21 @@ JOIN retail.order_items oi
     ON o.order_id = oi.order_id
 GROUP BY order_year, order_month
 ORDER BY order_year, order_month;
+
+WITH yearly_sales AS (
+    SELECT
+        EXTRACT(YEAR FROM o.order_date) AS order_year,
+        SUM(oi.qty * oi.price) AS total_revenue
+    FROM retail.orders o
+    JOIN retail.order_items oi
+        ON o.order_id = oi.order_id
+    GROUP BY order_year
+)
+
+SELECT
+    order_year,
+    total_revenue,
+    LAG(total_revenue) OVER (ORDER BY order_year) AS previous_year_revenue,
+    total_revenue
+    - LAG(total_revenue) OVER (ORDER BY order_year) AS revenue_growth
+FROM yearly_sales;
