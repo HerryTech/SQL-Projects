cb1355b84a1e25619e5d2ba83dda037b404b3904
Blessing <blessingilesanmi21@gmail.com>
2026-05-21 13:46:00 +0100

    Update Script

--- Retail_SQL_Project/Sql_Scripts/supply_chain_logistics_analysis.sql
+++ Retail_SQL_Project/Sql_Scripts/supply_chain_logistics_analysis.sql
@@ -6,19 +6,6 @@ FROM retail.shipments
 GROUP BY status
 ORDER BY total_shipments DESC;
 
-SELECT
-    ROUND(
-        COUNT(
-            CASE
-                WHEN status = 'Delivered'
-                THEN 1
-            END
-        ) * 100.0
-        / COUNT(*),
-        2
-    ) AS shipment_success_rate
-FROM retail.shipments;
-
 --Revenue by Supplier
 SELECT
     s.supplier_id,
